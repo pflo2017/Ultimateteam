e9c9b0ad39e37c72bd6388eb3e96c78c1c1976c4
Florin <prec.florin@gmail.com>
2025-06-21 14:38:32 +0300

    fix: remove hardcoded JWT token from function example

--- supabase/functions/fix-attendance-view/index.ts
+++ supabase/functions/fix-attendance-view/index.ts
@@ -117,7 +117,7 @@ serve(async (req) => {
   2. Make an HTTP request:
 
   curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/fix-attendance-view' \
-    --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+    --header 'Authorization: Bearer YOUR_SUPABASE_ANON_KEY_HERE' \
     --header 'Content-Type: application/json' \
     --data '{"name":"Functions"}'
 
