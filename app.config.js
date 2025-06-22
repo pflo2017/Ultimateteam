9368aebcdca73d850cbfcb30968f248d87aeca23
Florin <prec.florin@gmail.com>
2025-06-22 12:53:06 +0300

    fix: Hardcode Supabase credentials for published version

--- app.config.js
+++ app.config.js
@@ -33,8 +33,8 @@ export default {
       favicon: "./assets/favicon.png"
     },
     extra: {
-      supabaseUrl: process.env.SUPABASE_URL,
-      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+      supabaseUrl: "https://ulltpjezntzgiawchmaj.supabase.co",
+      supabaseAnonKey: "sb_publishable_k6gtnpe-RzS6RQ3EC8e5Jg_rVvZbqVm",
       eas: {
         projectId: "f43e8b35-5e2a-4e5d-92bb-9cdc051ca72a"
       }
